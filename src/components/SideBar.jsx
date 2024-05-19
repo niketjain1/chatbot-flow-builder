b8b7a7b3e4d9969175b4212445304f6cdcdf7f25
niketjain1 <niketj2000@gmail.com>
2024-05-19 22:52:01 +0530

    👌 IMPROVE: edit the size of the icon and fixed the alignment of side bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,11 +8,11 @@ const SideBar = () => {
     event.dataTransfer.effectAllowed = "move";
   };
 
-  const icon = <MessageSquareText />;
+  const icon = <MessageSquareText size={18} />;
   const text = "Message";
 
   return (
-    <aside className="flex flex-col items-start justify-start w-full border-l-2 p-4">
+    <aside className="fixed top-0 right-0 w-1/5 h-full bg-gray-100 border-gray-200 border-l-2 p-4 z-10">
       <button
         className="flex flex-col h-14 w-40 items-center justify-center border border-blue-400 p-2 rounded-lg"
         onDragStart={(event) => onDragStart(event, "input")}
