c8191cdfbdd64dfb2d0aa5153d0b56679ba9796b
niketjain1 <niketj2000@gmail.com>
2024-05-20 00:13:39 +0530

    📦 NEW: Added TextNode

---
+++ src/components/TextNode.jsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from "react";
+import { Handle, Position } from "reactflow";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { FaWhatsapp } from "react-icons/fa";
+
+const TextNode = ({ data, isConnectable }) => {
+  const onChange = useCallback((e) => {
+    console.log(e.target.value);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-14 w-40 bg-white rounded-lg shadow-xl">
+      <Handle
+        type="target"
+        position={Position.Left}
+        isConnectable={isConnectable}
+      />
+      <div className="flex flex-row h-6 w-full rounded-t-md border-b border-gray-300 bg-green-200 p-1">
+        <BiMessageSquareDetail
+          size={10}
+          className="flex items-center justify-center mr-1 mt-0.5"
+        />
+        <p className="roboto-regular">Send Message</p>
+        <FaWhatsapp size={10} className="ml-auto flex mr-1 mt-0.5" />
+      </div>
+      <div className="flex flex-auto">
+        <div type="text" className="w-full" onChange={onChange} />
+      </div>
+      <Handle
+        type="source"
+        position={Position.Right}
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+};
+
+export default TextNode;
