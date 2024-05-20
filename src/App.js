c1b8a9ce400a3d816b10c65e6bad1a9bbfd0755b
niketjain1 <niketj2000@gmail.com>
2024-05-20 17:26:55 +0530

    📦 NEW: Added the ReactFlow functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from "react";
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+} from "reactflow";
+import "reactflow/dist/style.css";
 
-function App() {
+import Sidebar from "./components/SideBar";
+import TextNode from "./components/TextNode";
+import SettingsPanel from "./components/SettingsPanel";
+
+const NodeTypes = {
+  textNode: TextNode,
+};
+
+const initialNodes = [
+  {
+    id: "0",
+    type: "textNode",
+    data: { label: "Test Node 1" },
+    position: { x: 250, y: 5 },
+  },
+];
+
+let id = 1;
+const getId = () => `${id++}`;
+
+const Flow = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [selectedNode, setSelectedNode] = useState(false);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      if (typeof type === "undefined" || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type: "textNode",
+        position,
+        data: { label: `Test Message ${id}` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance, setNodes]
+  );
+
+  const onNodeClick = useCallback((event, element) => {
+    console.log("Element", element.id);
+    setSelectedNode(element);
+  }, []);
+
+  const onTextChange = useCallback(
+    (id, value) => {
+      setNodes((nds) =>
+        nds.map((node) =>
+          node.id === id
+            ? { ...node, data: { ...node.data, label: value } }
+            : node
+        )
+      );
+    },
+    [setNodes]
+  );
+
+  const onSave = useCallback(() => {
+    setSelectedNode(null);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row" style={{ width: "200vh", height: "100vh" }}>
+      <ReactFlowProvider>
+        <div className="w-full h-full" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            onNodeClick={onNodeClick}
+            nodeTypes={NodeTypes}
+            fitView
+          >
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+        <SettingsPanel
+          selectedNode={selectedNode}
+          onTextChange={onTextChange}
+          onSave={onSave}
+        />
+      </ReactFlowProvider>
     </div>
   );
-}
+};
 
-export default App;
+export default Flow;
